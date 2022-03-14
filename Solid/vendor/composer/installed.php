ffc66a71e2895b0979ceb1064866a0dfb92c44e2
fabinhoizzy <fabinhoevil16@hotmail.com>
2022-03-14 20:42:38 -0300

    Começando um projeto com Solid bem basico

---
+++ Solid/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9a2faa10c40076a84fe510a0a9a3f24b47a02b47',
+        'name' => 'cviniciussdias/alura-solid',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cviniciussdias/alura-solid' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9a2faa10c40076a84fe510a0a9a3f24b47a02b47',
+            'dev_requirement' => false,
+        ),
+    ),
+);
