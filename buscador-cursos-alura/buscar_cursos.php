575e8bf85bdeab4e9489cbe8a3d16e7ce0d19a23
fabinhoizzy <fabinhoevil16@hotmail.com>
2022-03-23 13:53:53 -0300

    adicionando Script automatizado para teste

--- buscador-cursos-alura/buscar_cursos.php
+++ buscador-cursos-alura/buscar_cursos.php
@@ -1,6 +1,19 @@
 <?php
 
-use GuzzleHttp\Client;
+require_once 'vendor/autoload.php';
 
-$client = new Cliente();
-$client->request('GET', 'https://www.alura.com.br/formacao-desenvolvedor-php');
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+
+$crawler = new Crawler();
+
+$buscardor = new Buscador($client, $crawler);
+
+$cursos = $buscardor->buscar('cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo $curso . PHP_EOL;
+}
