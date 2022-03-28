9f978f2858b2fe9373f0cd5aa2b8f4bd6a79b695
fabinhoizzy <fabinhoevil16@hotmail.com>
2022-03-28 17:31:03 -0300

    Trabalhando com padrÃões criacionais

---
+++ DesignPatterns_padroesCracionais/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'ba34b845fae1380da5f81078cb7cd00bcf67b9cd',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'ba34b845fae1380da5f81078cb7cd00bcf67b9cd',
+            'dev_requirement' => false,
+        ),
+    ),
+);
