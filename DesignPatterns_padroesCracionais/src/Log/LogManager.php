3b3cf516fd24c52ff00f12fa50cf7cbeb8d8dc9f
fabinhoizzy <fabinhoevil16@hotmail.com>
2022-03-28 19:20:26 -0300

    Trabalhando com Logs

---
+++ DesignPatterns_padroesCracionais/src/Log/LogManager.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Alura\DesignPattern\Log;
+
+class LogManager
+{
+    public function log(string $severidade, string $mensagem): void
+    {
+
+    }
+}
---
+++ DesignPatterns_padroesCracionais/src/Log/LoggerWritter.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Alura\DesignPattern\Log;
+
+interface LoggerWritter
+{
+    public function escreve(string $mensagemFormatada): void;
+}
