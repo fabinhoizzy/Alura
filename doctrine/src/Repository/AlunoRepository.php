904969621bdefc4dd8e71d01a8ad1f4eb7b8317a
fabinhoizzy <fabinhoevil16@hotmail.com>
2022-04-05 20:42:55 -0300

    Trabalhando com MVC + PHP

--- doctrine/src/Repository/AlunoRepository.php
+++ doctrine/src/Repository/AlunoRepository.php
@@ -9,11 +9,14 @@ class AlunoRepository extends EntityRepository
 {
     public function buscaCursosPorAluno(): array
     {
-        $classeAluno = Aluno::class;
-        $entityManager = $this->getEntityManager();
-        $dql = "SELECT a, t, c FROM $classeAluno a JOIN a.telefones t JOIN a.cursos c";
-        $query = $entityManager->createQuery($dql);
+        $query = $this->createQueryBuilder('a')
+            ->join('a.telefones', 't')
+            ->join('a.cursos', 'c')
+            ->addSelect('t')
+            ->addSelect('c')
+        ->getQuery();
 
+        return $query->getResult();
     }
 
 }
