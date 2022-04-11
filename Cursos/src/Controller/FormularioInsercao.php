dc4e058e9c73be61623eb08b54bb54364c0d2f94
fabinhoizzy <fabinhoevil16@hotmail.com>
2022-04-11 18:38:30 -0300

    Criado aplicação para o desafio Backend da Alura

--- Cursos/src/Controller/FormularioInsercao.php
+++ Cursos/src/Controller/FormularioInsercao.php
@@ -2,11 +2,11 @@
 
 namespace Alura\Cursos\Controller;
 
-use Alura\Cursos\Helper\FlashMessageTrait;
+use Alura\Cursos\Helper\RenderizadorDeHtmlTrait;
 
 class FormularioInsercao implements InterfaceControladorRequisicao
 {
-    use FlashMessageTrait;
+    use RenderizadorDeHtmlTrait;
 
     public function processaRequisicao():void
     {
